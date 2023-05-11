ace6317b542ecbb5b5cb35e2bf01772c846b7be1
z06by <123419841+therealz06by@users.noreply.github.com>
2023-05-11 00:52:58 -0400

    xs

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-// Sample game data in JSON format
-var games = [
-    {
-      "id": 1,
-      "title": "Game 1",
-      "image": "game1.jpg",
-      "description": "Description of Game 1.",
-      "url": "game1.html"
-    },
-    // ... (Add the remaining game entries here)
-  ];
-  
-  // Function to generate the game HTML
-  function generateGameHTML(game) {
-    return `
-      <div class="game">
-        <img src="${game.image}" alt="${game.title}">
-        <h2>${game.title}</h2>
-        <p>${game.description}</p>
-        <a href="${game.url}">Play ${game.title}</a>
-      </div>
-    `;
-  }
-  
-  // Function to
+setTimeout(function() {
+  document.getElementById('loader').classList.add('hide');
+  setTimeout(function() {
+      document.getElementById('loader').style.display = 'none';
+  }, 100);
+}, 2000);
